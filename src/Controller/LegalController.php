625a299690ddba8d0db74457d5358b572f17dbed
Steelwix <mhunmael@hotmail.com>
2024-06-09 20:57:08 +0200

    Mise en place des mentions légales

---
+++ src/Controller/LegalController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\UX\Turbo\TurboBundle;
+
+class LegalController extends AbstractController
+{
+    #[Route('/legal', name: 'app_legal')]
+    public function index(Request $request): Response
+    {
+        $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
+        return $this->render('legal/index.html.twig');
+    }
+}
